3782d383af3634b40bda42feef2da33f78ed10fc
Hung Le <duchung.le195@gmail.com>
2022-06-26 11:16:37 +0700

    update report json

--- src/test/java/Resources/RunTest/RunTest.java
+++ src/test/java/Resources/RunTest/RunTest.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/java/resources/Feature/Login.feature"},
-        plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "pretty", "json:TestOutput/JsonReport/JsonReport.json"},
         dryRun = false,
-        glue = {"Resources.StepDefinitions.Hooks", "Resources.StepDefinitions", },
+        glue = {"Resources.StepDefinitions.Hooks", "Resources.StepDefinitions",},
         tags = "@sanity")
 public class RunTest extends AbstractTestNGCucumberTests {
 
