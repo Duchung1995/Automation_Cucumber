4fa51c1d3988b8e1e1e8d3093124a9e15ce7f1eb
Hung Le <duchung.le195@gmail.com>
2022-06-26 19:52:50 +0700

    update path

--- src/test/java/Resources/RunTest/RunTest.java
+++ src/test/java/Resources/RunTest/RunTest.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/java/resources/Feature/Login.feature"},
+@CucumberOptions(features = {"src\test\java\Resources\Feature\Login.feature"},
         plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "pretty", "json:TestOutput/JsonReport/JsonReport.json"},
         dryRun = false,
         glue = {"Resources.StepDefinitions.Hooks", "Resources.StepDefinitions",},
